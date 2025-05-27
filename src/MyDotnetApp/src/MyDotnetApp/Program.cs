94a7c4ba1102637573cfeebbe768fc18179e7869
Evgeniy Bessonov <bessonov.evgeniy@gmail.com>
2025-05-27 10:48:26 +0200

    Create Program.cs

---
+++ src/MyDotnetApp/src/MyDotnetApp/Program.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace MyDotnetApp
+{
+    public class Program
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("Hello from MyDotnetApp!");
+            return 0;
+        }
+    }
+}
