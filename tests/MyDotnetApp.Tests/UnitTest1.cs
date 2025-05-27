dc816737318438839383167a651101e7ab4d7cb5
Evgeniy Bessonov <bessonov.evgeniy@gmail.com>
2025-05-27 10:52:29 +0200

    Create UnitTest1.cs

---
+++ tests/MyDotnetApp.Tests/UnitTest1.cs
@@ -0,0 +1,15 @@
+using Xunit;
+using MyDotnetApp;
+
+namespace MyDotnetApp.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            // Simple test example, replace with real tests
+            Assert.True(true);
+        }
+    }
+}
